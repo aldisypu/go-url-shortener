f7a7c99dd1a94fe93c7a7ca469b4c858130ff81f
aldisypu <aldisypu@gmail.com>
2023-09-22 15:15:17 +0700

    Forwarding

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 
+	"github.com/aldisypu/go-url-shortener/handler"
+	"github.com/aldisypu/go-url-shortener/store"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,9 +12,20 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hey Go URL Shortener !",
+			"message": "Welcome to the URL Shortener API",
 		})
 	})
+
+	r.POST("/create-short-url", func(c *gin.Context) {
+		handler.CreateShortUrl(c)
+	})
+
+	r.GET("/:shortUrl", func(c *gin.Context) {
+		handler.HandleShortUrlRedirect(c)
+	})
+
+	// Note that store initialization happens here
+	store.InitializeStore()
 
 	err := r.Run(":9808")
 	if err != nil {
